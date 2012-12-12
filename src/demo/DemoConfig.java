94353c926eb72d5cd2d5fa24d0e950a5c8aeee60
asinbow.wang <asinbow.wang@saybot.com>
2012-12-12 14:59:07 +0800

    basic demo init

---
+++ src/demo/DemoConfig.java
@@ -0,0 +1,48 @@
+package demo;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.render.ViewType;
+
+import demo.model.User;
+
+public class DemoConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants me) {
+		me.setDevMode(true);
+		me.setViewType(ViewType.FREE_MARKER);
+	}
+
+	@Override
+	public void configHandler(Handlers me) {
+	}
+
+	@Override
+	public void configInterceptor(Interceptors me) {
+	}
+
+	@Override
+	public void configPlugin(Plugins me) {
+		//C3p0Plugin c3p0plugin = new C3p0Plugin(getProperty("jdbcUrl"), getProperty("user"), getProperty("pass"));
+		C3p0Plugin c3p0plugin = new C3p0Plugin("jdbc:mysql://localhost/saybot_vw", "saybot", "saybot");
+		
+		ActiveRecordPlugin activeRecordPlugin = new ActiveRecordPlugin(c3p0plugin);
+		activeRecordPlugin.addMapping("User", User.class);
+		
+		me.add(c3p0plugin);
+		me.add(activeRecordPlugin);
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+		me.add("/hello", HelloController.class);
+	}
+
+}
---
+++ src/demo/HelloController.java
@@ -0,0 +1,29 @@
+package demo;
+
+import com.jfinal.core.Controller;
+
+import demo.model.User;
+
+public class HelloController extends Controller {
+	
+	public void index() {
+		User user = null;
+		try {
+			int id = Integer.parseInt(getPara("id"));
+			user = User.dao.findById(id);
+		} catch (NumberFormatException e) {
+			String passport_id = getPara("passport_id");
+			user = User.dao.findFirst("select * from User where passport_id = ?", passport_id);
+		}
+		if (user!=null) {
+			renderJson(user);
+		} else {
+			renderText("Sorry, nothing found");
+		}
+	}
+	
+	public void report() {
+		renderText("Hello, Jfinal.\n" + getCookie("rock_format"));
+	}
+
+}
